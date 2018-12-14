e450294b64af5d69c0b6e27ae6b0d6ebc0c3131a
Adam Deacon <adeacon@findmypast.com>
2018-12-14 17:29:30 +0000

    Final project (WIP)

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -7,6 +7,13 @@ import pandas as pd
 from pandas.tools.plotting import scatter_matrix
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble.weight_boosting import AdaBoostClassifier
+from sklearn.cross_validation import train_test_split
+from itertools import compress
 
 pd.set_option('float_format', '{:f}'.format)
 pd.set_option('display.max_columns', 30)
@@ -18,66 +25,83 @@ from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
 import rubric
+NOSTATS=True
 
 
-def explore_dataset(data_dict, name=None, feature=None, createviz=False):
-
+def explore_dataset(features_list, data_dict, name=None, feature=None, createviz=None):
+	if NOSTATS:
+		return
+	print("\n\n")		
 	df = pd.DataFrame.from_dict(data_dict, orient='index')
 	df.replace({"NaN": np.nan}, inplace=True)
+	df = df[features_list]
 
 	if name:
-		print "\n\n"
-		print("Name: {0}...").format(name)
-		print df.loc[name]
+		print("\n\n")
+		print("Name: {0}...".format(name))
+		print(df.loc[name])
 		return
 
 	if feature:
-		print "\n\n"
-		print("Feature: {0}...").format(feature)
-		print df[df[feature].notnull()][[feature, "poi"]]
+		print("\n\n")
+		print("Feature: {0}...".format(feature))
+		print(df[df[feature].notnull()][[feature, "poi"]])
 		return
 
-	print "\n\nSHAPE..."
-	print "{0} rows and {1} columns".format(df.shape[0], df.shape[1])
+	print("\n\nSHAPE...")
+	print("{0} rows and {1} columns".format(df.shape[0], df.shape[1]))
 	feature_counter = len(df.columns) - 1 #exclude target variable
 	print("{0} features".format(feature_counter))
 	print("{0} POIs".format(df[df.poi].shape[0]))
-	print "\n\nINFO..."
+	print("\n\nINFO...")
 	df.info()
-	print "\n\nDESCRIBE..."
-	print df.describe(include="all")
+	print("\n\nDESCRIBE...")
+	print(df.describe(include="all"))
 
 	if not createviz:
 		return
 
-	print "\n\n"
+	print("\n\n")
 	num_fields = df.columns.values.tolist()
-	num_fields.remove("email_address")
-	num_fields.remove("poi")
+	try:
+		num_fields.remove("email_address")
+	except ValueError:
+		pass
+	try:
+		num_fields.remove("poi")
+	except ValueError:
+		pass
 	print("num_fields: {0}".format(num_fields))
 
-	print "\n\n"
+	print("\n\n")
 	print("Plot histograms for all numeric fields...")
 	df.hist(column=num_fields) #, bins=146)
-	plt.savefig('histograms.png')
+	plt.savefig('histograms_{0}.png'.format(createviz))
 
-	print "\n\n"
+	print("\n\n")
 	print("Plot scatter matrix for all numeric fields...")
 	scatter_matrix(df[num_fields], diagonal="kde")
-	plt.savefig('scatter_matrix.png')
+	plt.savefig('scatter_matrix_{0}.png'.format(createviz))
 
-	print "\n\n"
+	print("\n\n")
 	print("Plot box plots for all numeric fields by POI...")
 	df.boxplot(by="poi")
-	plt.savefig('boxplot.png')
+	plt.savefig('boxplot_{0}.png'.format(createviz))
 
 	plt.show()
 
 	return
 
 
-def add_features(data_dict):
+def remove_outliers(data_dict):
+	print("\n\n")
+	print("Removing TOTAL as not a person, just a spreadsheet artefact")
+	data_dict.pop("TOTAL", 0 )
+	return #data_dict
 
+
+def add_features(features_list, data_dict):
+	print("\n\n")
 	def computeFraction( poi_messages, all_messages ):
 	    """ given a number messages to/from POI (numerator) 
 	        and number of all messages to/from a person (denominator),
@@ -107,64 +131,134 @@ def add_features(data_dict):
 	    from_messages = data_point["from_messages"]
 	    fraction_to_poi = computeFraction( from_this_person_to_poi, from_messages )
 
-	    data_dict[name].update( {"from_poi_to_this_person":fraction_from_poi} )
-	    data_dict[name].update( {"from_this_person_to_poi":fraction_to_poi} )
+	    data_dict[name].update( {"fraction_from_poi":fraction_from_poi} )
+	    data_dict[name].update( {"fraction_to_poi":fraction_to_poi} )
 	    data_point["fraction_to_poi"] = fraction_to_poi
 
 	    if data_dict[name]["email_address"] == "NaN":
 	    	data_dict[name].update( {"has_email_address":0} )
 	    else:
 	    	data_dict[name].update( {"has_email_address":1} )
+
+	new_features_list = features_list+["fraction_from_poi", "fraction_to_poi", "has_email_address"]
+	print("Added new features: fraction_from_poi, fraction_to_poi, has_email_address")
 	    
-	# print data_dict
-	return data_dict
+	# print(data_dict)
+	return new_features_list
+
+
+def select_features(features_list, my_dataset):
+	print("\n\n")
+	print("Removing email_address as not numeric or really a useful feature anymore")
+	features_list.remove("email_address")
+
+	### Extract features and labels from dataset for local testing
+	data = featureFormat(my_dataset, features_list, sort_keys = True)
+	labels, features = targetFeatureSplit(data)
+
+	clf = tree.DecisionTreeClassifier(random_state=1)
+	clf.fit(features, labels)
+
+	kbest = 0
+	selected_features_list = ["poi"] ## always included
+	print("Finding most important features...")
+	for n, imp in enumerate(clf.feature_importances_):
+		print "{0}, {1}, {2}".format(n, features_list[n+1], imp)
+		if imp > 0.0:
+			kbest+=1
+			selected_features_list.append(features_list[n+1])
+
+	print("Selecting {0} best features...".format(kbest))
+	selector = SelectKBest(f_classif, k=kbest)
+	selector.fit(features, labels)
+
+	# selector_list = [True]+selector.get_support().tolist() ## so poi always included
+
+	# selected_features_list = list(compress(features_list, selector_list))
+	selected_features = selector.transform(features)
+
+	print("Selected {0} features".format(len(selected_features_list)))
+	print("Selected features are: {0}".format(selected_features_list))
+
+	return selected_features_list, labels, selected_features
+
+
+def select_algorithm(labels, features):
+	print("\n\n")
+	# Split testing and training data
+		# test_size
+	# Use different algorithms (2+)
+		# Naive Bayes
+		# Support Vector Machines
+	# Tune parameters (try 3+, use 1+)
+		# GridSearchCV
+	# Evaluation metric (2+)
+		# Precision > 0.3
+		# Recall > 0.3
+
+	sizes = [0.1, 0.2, 0.3, 0.4, 0.5]
+	algos = [GaussianNB(), SVC(kernel='rbf'), AdaBoostClassifier(n_estimators=10)]
+
+	for size in sizes:
+		for algo in algos:
+			name = algo.__doc__[:24].strip()
+			print("Attempting with test_size={0}, using {1}...".format(size, name))
+			features_train, features_test, labels_train, labels_test = \
+			    train_test_split(features, labels, test_size=size, random_state=42)
+
+			clf = algo
+			clf.fit(features_train, labels_train)
+
+			print("SCORE: {0}".format(clf.score(features_test, labels_test)))
+
+	return clf
 
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
+# features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees', 'to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi'] ## from project docs
+
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
-    data_dict = pickle.load(data_file)
+	data_dict = pickle.load(data_file)
+
 # rubric.r01_functionality()
 # rubric.r02_usability()
 # rubric.r03_data_exploration()
-explore_dataset(data_dict, createviz=False)
-explore_dataset(data_dict, feature="loan_advances")
-explore_dataset(data_dict, feature="director_fees")
-explore_dataset(data_dict, feature="email_address")
+explore_dataset(features_list, data_dict, createviz="initial")
+explore_dataset(features_list, data_dict, feature="loan_advances")
+explore_dataset(features_list, data_dict, feature="director_fees")
+explore_dataset(features_list, data_dict, feature="email_address")
 
 ### Task 2: Remove outliers
 # rubric.r04_outlier_investigation()
-explore_dataset(data_dict, name="TOTAL")
-data_dict.pop("TOTAL", 0 )
+explore_dataset(features_list, data_dict, name="TOTAL")
+remove_outliers(data_dict)
 
 ### Task 3: Create new feature(s)
 # rubric.r05_create_new_features()
-data_dict = add_features(data_dict)
-explore_dataset(data_dict, createviz=True)
-
-rubric.r06_intelligently_select()
-rubric.r07_properly_scale()
+features_list = add_features(features_list, data_dict)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
-# features_list = ['poi','salary'] # You will need to use more features
-features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees', 'to_messages', 'email_address', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
-### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
-labels, features = targetFeatureSplit(data)
+features_list, labels, features = select_features(features_list, my_dataset)
+explore_dataset(features_list, data_dict, createviz="final")
+# rubric.r06_intelligently_select()
+rubric.r07_properly_scale()
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
-rubric.r08_pick_an_algorithm()
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+# from sklearn.naive_bayes import GaussianNB
+# clf = GaussianNB()
+clf = select_algorithm(labels, features)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -172,13 +266,15 @@ clf = GaussianNB()
 ### function. Because of the small size of the dataset, the script uses
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-rubric.r09_discuss_parameter()
-rubric.r10_tune_the_algorithm()
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+# from sklearn.cross_validation import train_test_split
+# features_train, features_test, labels_train, labels_test = \
+#     train_test_split(features, labels, test_size=0.3, random_state=42)
+
+rubric.r08_pick_an_algorithm()
+rubric.r09_discuss_parameter()
+rubric.r10_tune_the_algorithm()
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
--- final_project/rubric.py
+++ final_project/rubric.py
@@ -1,6 +1,3 @@
-# import logging
-
-# logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def r01_functionality():
 	"""
@@ -8,9 +5,9 @@ def r01_functionality():
 	-------------------------------
 	Code reflects the description in the answers to questions in the writeup. i.e. code performs the functions documented in the writeup and the writeup clearly specifies the final analysis strategy.
 	"""
-	print("\n"+r01_functionality.__doc__)
+	# print("\n"+r01_functionality.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r02_usability():
@@ -19,12 +16,12 @@ def r02_usability():
 	-------------------------------
 	poi_id.py can be run to export the dataset, list of features and algorithm, so that the final algorithm can be checked easily using tester.py.
 	"""
-	print("\n"+r02_usability.__doc__)
+	# print("\n"+r02_usability.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
-def r03_data_exploration(data_dict):
+def r03_data_exploration():
 	"""
 	Understanding the Dataset and Question - Data Exploration (related lesson: Datasets and Questions)
 	-------------------------------
@@ -36,9 +33,9 @@ def r03_data_exploration(data_dict):
 	    are there features with many missing values? etc.
 
 	"""
-	print("\n"+r03_data_exploration.__doc__)
+	# print("\n"+r03_data_exploration.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r04_outlier_investigation():
@@ -47,9 +44,9 @@ def r04_outlier_investigation():
 	-------------------------------
 	Student response identifies outlier(s) in the financial data, and explains how they are removed or otherwise handled.
 	"""
-	print("\n"+r04_outlier_investigation.__doc__)
+	# print("\n"+r04_outlier_investigation.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r05_create_new_features():
@@ -58,9 +55,9 @@ def r05_create_new_features():
 	-------------------------------
 	At least one new feature is implemented. Justification for that feature is provided in the written response. The effect of that feature on final algorithm performance is tested or its strength is compared to other features in feature selection. The student is not required to include their new feature in their final feature set.
 	"""
-	print("\n"+r05_create_new_features.__doc__)
+	# print("\n"+r05_create_new_features.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r06_intelligently_select():
@@ -69,9 +66,9 @@ def r06_intelligently_select():
 	-------------------------------
 	Univariate or recursive feature selection is deployed, or features are selected by hand (different combinations of features are attempted, and the performance is documented for each one). Features that are selected are reported and the number of features selected is justified. For an algorithm that supports getting the feature importances (e.g. decision tree) or feature scores (e.g. SelectKBest), those are documented as well.
 	"""
-	print("\n"+r06_intelligently_select.__doc__)
+	# print("\n"+r06_intelligently_select.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r07_properly_scale():
@@ -80,9 +77,9 @@ def r07_properly_scale():
 	-------------------------------
 	If algorithm calls for scaled features, feature scaling is deployed.
 	"""
-	print("\n"+r07_properly_scale.__doc__)
+	# print("\n"+r07_properly_scale.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r08_pick_an_algorithm():
@@ -91,9 +88,9 @@ def r08_pick_an_algorithm():
 	-------------------------------
 	At least two different algorithms are attempted and their performance is compared, with the best performing one used in the final analysis.
 	"""
-	print("\n"+r08_pick_an_algorithm.__doc__)
+	# print("\n"+r08_pick_an_algorithm.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r09_discuss_parameter():
@@ -102,9 +99,9 @@ def r09_discuss_parameter():
 	-------------------------------
 	Response addresses what it means to perform parameter tuning and why it is important.
 	"""
-	print("\n"+r09_discuss_parameter.__doc__)
+	# print("\n"+r09_discuss_parameter.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r10_tune_the_algorithm():
@@ -118,9 +115,9 @@ def r10_tune_the_algorithm():
 	    Parameter tuning incorporated into algorithm selection (i.e. parameters tuned for more than one algorithm, and best algorithm-tune combination selected for final analysis).
 
 	"""
-	print("\n"+r10_tune_the_algorithm.__doc__)
+	# print("\n"+r10_tune_the_algorithm.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r11_usage_of_evaluation():
@@ -129,9 +126,9 @@ def r11_usage_of_evaluation():
 	-------------------------------
 	At least two appropriate metrics are used to evaluate algorithm performance (e.g. precision and recall), and the student articulates what those metrics measure in context of the project task.
 	"""
-	print("\n"+r11_usage_of_evaluation.__doc__)
+	# print("\n"+r11_usage_of_evaluation.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r12_discuss_validation():
@@ -140,9 +137,9 @@ def r12_discuss_validation():
 	-------------------------------
 	Response addresses what validation is and why it is important.
 	"""
-	print("\n"+r12_discuss_validation.__doc__)
+	# print("\n"+r12_discuss_validation.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r13_validation_strategy():
@@ -151,9 +148,9 @@ def r13_validation_strategy():
 	-------------------------------
 	Performance of the final algorithm selected is assessed by splitting the data into training and testing sets or through the use of cross validation, noting the specific type of validation performed.
 	"""
-	print("\n"+r13_validation_strategy.__doc__)
+	# print("\n"+r13_validation_strategy.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
 def r14_algorithm_performance():
@@ -162,8 +159,8 @@ def r14_algorithm_performance():
 	-------------------------------
 	When tester.py is used to evaluate performance, precision and recall are both at least 0.3.
 	"""
-	print("\n"+r14_algorithm_performance.__doc__)
+	# print("\n"+r14_algorithm_performance.__doc__)
 
-	raise SystemExit(0) #exit early
+	#raise SystemExit(0) #exit early
 
 
